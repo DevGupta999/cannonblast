71d367047bcb4e3aec91aa9c2b986abe4f63029e
DevGupta999 <105483467+DevGupta999@users.noreply.github.com>
2022-05-23 14:11:41 +0530

    cannonblast

---
+++ MissileCommand/Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    public static float selector=1;
+
+
+    
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad>200)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        Debug.Log(Time.timeSinceLevelLoad);
+
+
+    }
+
+
+}
---
+++ MissileCommand/Assets/Scripts/aspectManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aspectManager : MonoBehaviour
+{
+    public SpriteRenderer ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float orthoSize = ground.bounds.size.x * Screen.height / Screen.width * 0.5f;
+        Camera.main.orthographicSize = orthoSize;
+    }
+
+    
+}
